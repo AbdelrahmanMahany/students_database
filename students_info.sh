8b47b9a0c11668131ec499f4af9469846a362061
Abdelrahman Mahany <138822186+AbdelrahmanMahany@users.noreply.github.com>
2023-09-20 18:07:08 +0300

    Update students_info.sh

--- students_info.sh
+++ students_info.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 # Info about my computer science students from students database
 
 echo -e "\n~~ My Computer Science Students ~~\n"
@@ -6,9 +7,11 @@ echo -e "\n~~ My Computer Science Students ~~\n"
 PSQL="psql -X --username=freecodecamp --dbname=students --no-align --tuples-only -c"
 
 echo -e "\nFirst name, last name, and GPA of students with a 4.0 GPA:"
-echo "$($PSQL "select first_name, last_name, gpa from students where gpa =4.0")"
+echo "$($PSQL "SELECT first_name, last_name, gpa FROM students WHERE gpa = 4.0")"
 
-echo -e "\nAll course names whose first letter is before 'D' in the alphabet:" 
-echo "$($PSQL "select course from courses where course < 'D'")"
+echo -e "\nAll course names whose first letter is before 'D' in the alphabet:"
+echo "$($PSQL "SELECT course FROM courses WHERE course < 'D'")"
 
 echo -e "\nFirst name, last name, and GPA of students whose last name begins with an 'R' or after and have a GPA greater than 3.8 or less than 2.0:"
+echo "$($PSQL "SELECT first_name, last_name, gpa FROM students WHERE last_name >= 'R' and (gpa > 3.8 or gpa < 2.0)")"
+
